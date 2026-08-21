2fef7cc4ba5a4826cfe6bdc1debda2372564ccc4
Nehal Solanki <117564486+nehalss267@users.noreply.github.com>
2026-08-21 21:10:53 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        //XYYX
+        //2 ->2 Y replaced with X //2 can be atmost k
+        //maximize sliding window 
+        //longest window (ensure window: with same char)-> longest substring
+        int max=Integer.MIN_VALUE;
+        Set<Character> charSet=new HashSet<>();
+        for(char c:s.toCharArray()){
+            charSet.add(c);
+        }
+        //s.toCharArray() use instead of set ,gives tle(o(n*m);n=#charArray elem) error we just wnat disticnt char(no duplicates) so use set
+        for(char c:charSet){
+            //initialize window
+            int l=0;
+            int count=0;//cnt of c
+            for(int r=0;r<s.length();r++){
+                if(s.charAt(r)==c){
+                    count++;
+                }
+
+                while((r-l+1)-count>k){
+                    if(s.charAt(l)==c){
+                        count--;
+                    }
+                    l++;
+                }
+                // while(l>0 && s.charAt(l-1)==c){
+                //     l--;
+                // }
+                max=Math.max((r-l+1),max);
+
+            }
+        }
+        return max;
+    }
+}
