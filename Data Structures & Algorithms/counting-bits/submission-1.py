3e5a8c2c5a5c91b8ffe726ee7ef8241fc980d3ed
Nehal Solanki <117564486+nehalss267@users.noreply.github.com>
2026-09-23 23:37:28 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp=[0]*(n+1)
+        for i in range(len(dp)):
+            dp[i]=dp[i>>1]+(i&1)
+        return dp
+        # t(n)=O(n)
+        # s(n)=O(n) for array
